44fde6ad2119325a15a59050871e8260bb3ee485
alexcere <48130030+alexcere@users.noreply.github.com>
2023-10-06 20:41:16 +0200

    Move var to top

--- greedy/algorithm.py
+++ greedy/algorithm.py
@@ -16,20 +16,22 @@ instr_T = Dict[str, Any]
 local_index_T = int
 
 
-def select_local_from_value(value: var_T, locals_: List[var_T]) -> local_index_T:
+def idx_wrt_cstack(idx: int, cstack: List, fstack: List) -> int:
     """
-    Returns the first local in the list which contains the corresponding value. Otherwise, raises an Exception.
+    Given a position w.r.t fstack, returns the corresponding position w.r.t cstack
     """
-    return locals_.index(value)
+    return idx - len(fstack) + len(cstack)
 
 
-def set_local_value(x: int, cstack: List[var_T], clocals: List[var_T]) -> Tuple[List[var_T], List[var_T]]:
+def idx_wrt_fstack(idx: int, cstack: List, fstack: List) -> int:
     """
-    Returns the state of the stack and locals after storing the top of the stack in the local with index x
+    Given a position w.r.t cstack, returns the corresponding position w.r.t fstack
     """
-    elem = cstack.pop(0)
-    clocals[x] = elem
-    return cstack, clocals
+    return idx - len(cstack) + len(fstack)
+
+
+def top_relative_position_to_fstack(cstack: List[var_T], fstack: List[var_T]) -> int:
+    return len(fstack) - len(cstack)
 
 
 def extract_idx_from_id(instr_id: str) -> int:
@@ -77,11 +79,14 @@ class SymbolicState:
         """
         return [self.var_uses[initial_var] < self.total_uses[initial_var] for initial_var in self.locals]
 
-    def select_local_from_value(self, value: var_T) -> local_index_T:
+    def local_with_value(self, value: var_T) -> local_index_T:
         """
-        Returns the first local in the list which contains the corresponding value. Otherwise, raises an Exception.
+        Returns the first local in the list which contains the corresponding value. Otherwise, returns -1.
         """
-        return self.locals.index(value)
+        try:
+            return self.locals.index(value)
+        except:
+            return -1
 
     def lset(self, x: int, in_position: bool) -> None:
         """
@@ -166,10 +171,26 @@ class SymbolicState:
     def top_stack(self) -> Optional[var_T]:
         return None if len(self.stack) == 0 else self.stack[0]
 
+    def available_local(self) -> int:
+        """
+        Choose an available local to store an element. Otherwise, introduces an extra local to store it
+        :return:
+        """
+        # By reversing the liveness list, locals that have been introduced are prioritized over the ones that contain
+        # valid values for the final stack
+        # TODO: maybe this decision should also consider the final state somehow
+        for x, is_live in enumerate(reversed(self.liveness)):
+            if not is_live:
+                return len(self.liveness) - x - 1
+
+        # We just introduce an empty element and initialize the live variable to False
+        self.locals.append('')
+        self.liveness.append(False)
+        return len(self.locals) - 1
+
     def __repr__(self):
         sentences = [f"Current stack: {self.stack}", "Current locals:", *(f"{local}: {liveness}" for local, liveness in zip(self.locals, self.liveness))]
         return '\n'.join(sentences)
-
 
 
 class SMSgreedy:
@@ -237,7 +258,7 @@ class SMSgreedy:
 
         return var_uses
 
-    def _compute_var2pos(self):
+    def _compute_var2pos(self) -> Dict[var_T, List[Tuple[Location, int]]]:
         var2pos = defaultdict(lambda: [])
         for i, stack_var in enumerate(self._final_stack):
             var2pos[stack_var].append((Location.stack, i))
@@ -299,6 +320,60 @@ class SMSgreedy:
         assert instr_id is not None
         return True
 
+    def _place_stack_elem_in_position(self, cstate: SymbolicState, positions: List[int]) -> List[id_T]:
+        """
+        Given a the set of positions an element must appear in a stack, it places them in their corresponding
+        position by traversing the stack, storing the intermediate elements and loading them
+        in reversed order.
+        """
+        ops = []
+        deepest_position = max(positions)
+        for i in range(deepest_position + 1):
+            # If the element is already stored in a local, we just drop it
+            if cstate.local_with_value(cstate.stack[i]) != -1:
+                cstate.drop()
+                ops.append('POP')
+            else:
+                x = cstate.available_local()
+
+                # We need to set it and not tee because we have to 'remove' it from the stack. It cannot be in its
+                # position because in that case it'd have been stored previously
+                cstate.lset(x, False)
+                ops.append(f"LSET_{x}")
+
+        idx = top_relative_position_to_fstack(cstate.stack, self._final_stack)
+        local_to_retrieve = cstate.local_with_value(self._final_stack[idx])
+        # Then we place the elements from locals to the stack when possible
+        while idx >= 0 and local_to_retrieve != -1:
+            cstate.lget(local_to_retrieve)
+            ops.append(f"LGET_{local_to_retrieve}")
+            idx -= 1
+            local_to_retrieve = cstate.local_with_value(self._final_stack[idx])
+        return ops
+
+    def _available_positions(self, var_elem: var_T, cstate: SymbolicState) -> Tuple[List[int], List[int]]:
+        """
+        Returns the set of positions w.r.t stack and locals resp. in which we need to store the var elem
+        """
+        positions = self._var2pos[var_elem]
+
+        positions_available_stack = []
+        positions_available_locals = []
+
+        # We determine in which positions both in the stack (enough elements) and in the locals the element
+        # can be placed at this moment
+        for location, x in positions:
+            if location == Location.stack:
+                idx_cstack = idx_wrt_cstack(x, cstate.stack, self._final_stack)
+                # Corresponding position in cstack
+                if idx_cstack >= 0:
+                    positions_available_locals.append(idx_cstack)
+            else:
+                if cstate.locals[x] != var_elem and not cstate.liveness[x]:
+                    positions_available_locals.append(x)
+
+        return positions_available_stack, positions_available_locals
+
     def split_ids_into_categories(self) -> Tuple[Set[id_T], List[id_T], Set[id_T], Set[id_T]]:
         """
         Returns four sets of instruction ids: the ones that have some kind of dependency (mops), the ones that appear in
@@ -329,11 +404,16 @@ class SMSgreedy:
         # We must extract the order that only includes ids from mops
         return [id_ for id_ in topo_order if id_ in mops]
 
-    def is_in_position_stack(self, var_elem: var_T, cstate: SymbolicState) -> bool:
-        # Var elem has position 0 in cstack, hence why new pos ic computed this way
-        new_pos = len(cstate.stack) - len(self._final_stack)
-        # Check it is within range and
-        return 0 <= new_pos < len(self._final_stack) and self._final_stack[new_pos] == var_elem
+    def var_must_be_moved(self, var_elem: var_T, cstate: SymbolicState) -> bool:
+        """
+        By construction, a var element must be moved if it appears in no locals, unless it has one position tied which
+        corresponds to current top of the stack
+        """
+        positions = self._var2pos[var_elem]
+        local = cstate.local_with_value(var_elem)
+
+        assert len(positions) > 0, f'Variable {var_elem} does not appear anywhere else!'
+        return local == -1 or len(positions) > 1 or positions[0][0] != 'stack' or idx_wrt_cstack(positions[0][1], cstate.stack, self._final_stack) != 0
 
     def can_be_placed_in_position(self, var_elem: var_T, clocals: List[var_T], clocals_liveness: List[bool]) -> bool:
         """
@@ -346,11 +426,46 @@ class SMSgreedy:
 
     def move_top_to_position(self, var_elem: var_T, cstate: SymbolicState) -> List[id_T]:
         """
-        Move top to position assumes the stack element is not in their corresponding position.
-        If it must appear more than once, we move it to a local to duplicate it (checking if it appears in flocals).
-        Otherwise,
+        Tries to store current element in all the positions in which it is available to be moved
         """
-        pass
+        ops = []
+        positions_available_locals, positions_available_stack = self._available_positions(var_elem, cstate)
+
+        if len(positions_available_locals) == 0:
+            # No position is available yet, so we just store it one available local
+            y = cstate.available_local()
+            cstate.lset(y, False)
+            ops.append(f"LSET_{y}")
+
+            # If it needs to be stored somewhere in the stack, we perform that step at this point
+            if len(positions_available_stack) > 0:
+                self._place_stack_elem_in_position(cstate, positions_available_stack)
+
+        else:
+            # There is at least one local in fstate in which we can place the state. We first store the
+            # value in all locals with several ltee until the last one, which can be either a lset or ltee
+            i = 0
+            while i < len(positions_available_locals) - 1:
+                local = positions_available_locals[i]
+                cstate.ltee(local, True)
+                ops.append(f"LTEE_{local}")
+                i += 1
+
+            # Finally, we check whether the last store to local can be a ltee (if there is an element left to place in
+            # the stack at the same position) or a lset
+            if len(positions_available_stack) == 1 and idx_wrt_cstack(positions_available_stack[0], cstate.stack, self._final_stack) == 0:
+                local = positions_available_locals[i]
+                cstate.ltee(local, True)
+                ops.append(f"LTEE_{local}")
+            else:
+                y = cstate.available_local()
+                cstate.lset(y, False)
+                ops.append(f"LSET_{y}")
+
+                if len(positions_available_stack) > 0:
+                    self._place_stack_elem_in_position(cstate, positions_available_stack)
+
+        return ops
 
     def choose_next_computation(self, mops: List[id_T], sops: List[id_T], rops: Set[id_T],
                                 cstack: List[var_T], clocals: List[var_T], clocals_liveness: List[bool]) -> id_T:
@@ -440,8 +555,8 @@ class SMSgreedy:
                 cstate.drop()
                 optg.append("drop")
 
-            # Top of the stack must be placed in the position it appears in the fstack or flocals
-            elif var_top is not None and not self.is_in_position_stack(var_top, cstate):
+            # Top of the stack must be placed in some other position
+            elif var_top is not None and self.var_must_be_moved(var_top, cstate):
                 optg.extend(self.move_top_to_position(var_top, cstate))
 
             # Top of the stack cannot be moved to the corresponding position. As there is always the possibility
